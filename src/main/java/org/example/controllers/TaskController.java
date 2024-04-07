480da42582a3a33ed89d70d919f4e8262011ce9a
Ilya Vlasyuk <vlasuyk.2015.1@mail.ru>
2024-04-07 15:49:55 +0300

    Initial commit

---
+++ src/main/java/org/example/controllers/TaskController.java
@@ -0,0 +1,69 @@
+package org.example.controllers;
+
+import com.example.sem3HomeTask.domain.User;
+import com.example.sem3HomeTask.services.DataProcessingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private DataProcessingService service;
+
+    @GetMapping
+    public List<String> getAllTasks() {
+        List<String> tasks = new ArrayList<>();
+        tasks.add("sort");
+        tasks.add("filter");
+        tasks.add("calc");
+        return tasks;
+    }
+
+    /**
+     * Endpoint to retrieve a sorted list of users based on age.
+     * localhost:8080/tasks/sort
+     *
+     * @return A list of users sorted by age.
+     */
+    @GetMapping("/sort")
+    public List<User> sortUsersByAge() {
+        return service.sortUsersByAge(service.getRepository().getUsers());
+    }
+
+
+    /**
+     * Endpoint for filtering users by age.
+     * localhost:8080/tasks/filter/25
+     *
+     * @param age The age to filter users by.
+     * @return A list of users matching the specified age.
+     */
+    @GetMapping("/filter/{age}")
+    public List<User> filterUsersByAge(@PathVariable("age") int age) {
+        List<User> users = service.getRepository().getUsers();
+        return service.filterUsersByAge(users, age);
+    }
+
+
+    /**
+     * Endpoint to calculate the average age of users.
+     * localhost:8080/tasks/calc
+     *
+     * @return The average age of all users.
+     */
+    @GetMapping("/calc")
+    public double calculateAverageAge() {
+        List<User> users = service.getRepository().getUsers();
+        return service.calculateAverageAge(users);
+    }
+
+}
---
+++ src/main/java/org/example/services/RegistrationService.java
@@ -0,0 +1,32 @@
+package org.example.services;
+
+import com.example.sem3HomeTask.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private NotificationService notificationService;
+    @Autowired
+    private DataProcessingService dataProcessingService;
+
+    public RegistrationService() {
+    }
+
+    public void processRegistration(User user) {
+        dataProcessingService.getRepository().saveUser(user);
+        notificationService.sendNotification("User created and added to list");
+    }
+
+    public DataProcessingService getDataProcessingService() {
+        return dataProcessingService;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+}
